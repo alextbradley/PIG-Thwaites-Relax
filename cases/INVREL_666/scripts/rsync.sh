c3b5a83235e619e35d199dec6308d01a601c560e
not populated <aleey@bas.ac.uk>
2022-03-28 15:11:06 +0100

    update case name in rsync

--- cases/INVREL_666/scripts/rsync.sh
+++ cases/INVREL_666/scripts/rsync.sh
@@ -6,7 +6,7 @@ JOBNO=666
 cd ../run
 
 # make target directory
-W_HOMEDIR=$W_HOMEROOT/ATTR_${JOBNO}/run
+W_HOMEDIR=$W_HOMEROOT/INVREL_${JOBNO}/run
 ssh $W_HOMEHOST "mkdir -p $W_HOMEDIR"
 
 rsync -avzL *.nc $W_HOMEHOST:$W_HOMEDIR
